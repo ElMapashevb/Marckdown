def6f68cfde486ab0c4e8de6ca97186fc366e653
ElMapashevb <junoliva05@gmail.com>
2025-12-05 23:14:45 -0300

    Update db.js to use environment variables
    Refactor database connection to use environment variables for configuration.

--- db.js
+++ db.js
@@ -1,31 +1,31 @@
-// db.js
 const mysql = require('mysql2/promise');
+
+const DB_HOST = process.env.DB_HOST;
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const DB_NAME = process.env.DB_NAME;
 
 let pool;
 
 async function initDb() {
   pool = mysql.createPool({
-    host: 'yamanote.proxy.rlwy.net',
-    port: 51438,
-    user: 'root',
-    password: 'nUgYseHcgmmAlxlqVfIdJchgtzSbhIIL',
-    database: 'railway',
+    host: DB_HOST,
+    user: DB_USER,
+    password: DB_PASSWORD,
+    database: DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
     queueLimit: 0,
-    charset: 'utf8mb4',
-    ssl: {
-      rejectUnauthorized: false
-    }
+    charset: 'utf8',
+    ssl: { rejectUnauthorized: false }
   });
-
-  await pool.query('SELECT 1');
-  console.log('✅ Conectado a MySQL Railway');
+  console.log("✅ Conexión con la base de datos establecida");
 }
 
 function getPool() {
-  if (!pool) throw new Error('❌ DB no inicializada');
+  if (!pool) throw new Error("DB no inicializada");
   return pool;
 }
 
 module.exports = { initDb, getPool };
+
